598eed0fb10ea2f8a2eee3bbcce80d171538c880
Emilio Nicoli <emilionicoli@gmail.com>
2020-12-03 16:49:23 -0700

    first run

---
+++ SwiftUIViews/SwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsApp.swift
+//  SwiftUIViews
+//
+//  Created by Emilio Nicoli on 12/3/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
